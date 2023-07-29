fc419033c792dd4c25470b34f6150b50b9413354
sercan <isk.sercan@gmail.com>
2023-07-29 18:32:07 +0300

    Added ReportsController

--- PhoneBookMicroservices/Controllers/ReportsController.cs
+++ PhoneBookMicroservices/Controllers/ReportsController.cs
@@ -1,6 +1,101 @@
-﻿namespace PhoneBookMicroservices.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PhoneBookMicroservices.Models;
+
+namespace PhoneBookMicroservices.Controllers
 {
-    public class ReportsController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportsController : ControllerBase
     {
+        private readonly ContactDirectoryContext _context;
+
+        public ReportsController(ContactDirectoryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/reports
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Report>>> GetReports()
+        {
+            return await _context.Reports.ToListAsync();
+        }
+
+        // GET: api/reports/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Report>> GetReport(Guid id)
+        {
+            var report = await _context.Reports.FindAsync(id);
+
+            if (report == null)
+            {
+                return NotFound();
+            }
+
+            return report;
+        }
+
+        // POST: api/reports
+        [HttpPost]
+        public async Task<ActionResult<Report>> CreateReport(Report report)
+        {
+            _context.Reports.Add(report);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetReport), new { id = report.Id }, report);
+        }
+
+        // PUT: api/reports/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateReport(Guid id, Report report)
+        {
+            if (id != report.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(report).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReportExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/reports/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteReport(Guid id)
+        {
+            var report = await _context.Reports.FindAsync(id);
+
+            if (report == null)
+            {
+                return NotFound();
+            }
+
+            _context.Reports.Remove(report);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ReportExists(Guid id)
+        {
+            return _context.Reports.Any(e => e.Id == id);
+        }
     }
 }
