530bfbf887eecaa67589d4db58fd9e43117256da
sercan <isk.sercan@gmail.com>
2023-07-29 18:35:29 +0300

    Added ContactsController

--- PhoneBookMicroservices/Controllers/ContactsController.cs
+++ PhoneBookMicroservices/Controllers/ContactsController.cs
@@ -1,6 +1,96 @@
-﻿namespace PhoneBookMicroservices.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PhoneBookMicroservices.Models;
+
+namespace PhoneBookMicroservices.Controllers
 {
-    public class ContactsController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
     {
+        private readonly ContactDirectoryContext _context;
+
+        public ContactsController(ContactDirectoryContext context)
+        {
+            _context = context;
+        }
+        // GET: api/contacts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contact>>> GetContacts()
+        {
+            return await _context.Contacts.ToListAsync();
+        }
+
+        // GET: api/contacts/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contact>> GetContact(Guid id)
+        {
+            var contact = await _context.Contacts.FindAsync(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            return contact;
+        }
+
+        // POST: api/contacts
+        [HttpPost]
+        public async Task<ActionResult<Contact>> CreateContact(Contact contact)
+        {
+            _context.Contacts.Add(contact);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetContact), new { id = contact.Id }, contact);
+        }
+
+        // PUT: api/contacts/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateContact(Guid id, Contact contact)
+        {
+            if (id != contact.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(contact).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContactExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/contacts/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteContact(Guid id)
+        {
+            var contact = await _context.Contacts.FindAsync(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            _context.Contacts.Remove(contact);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        private bool ContactExists(Guid id)
+        {
+            return _context.Contacts.Any(e => e.Id == id);
+        }
+
     }
 }
