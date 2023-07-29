b08a73644cc793cb8a6f4481fc4350976b527aa7
sercan <isk.sercan@gmail.com>
2023-07-29 17:58:23 +0300

    Added DbContext to services

--- PhoneBookMicroservices/Program.cs
+++ PhoneBookMicroservices/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using PhoneBookMicroservices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ContactDirectoryContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
