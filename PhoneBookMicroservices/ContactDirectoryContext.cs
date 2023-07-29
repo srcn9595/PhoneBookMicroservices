7f73b8bac595f9502bdec8ba50e29b4e941aa3d4
sercan <isk.sercan@gmail.com>
2023-07-29 17:54:45 +0300

    Added DbContext for database operations

---
+++ PhoneBookMicroservices/ContactDirectoryContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneBookMicroservices.Models;
+
+namespace PhoneBookMicroservices
+{
+    public class ContactDirectoryContext : DbContext
+    {
+        public ContactDirectoryContext(DbContextOptions<ContactDirectoryContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<ContactDetail> ContactDetails { get; set; }
+        public DbSet<Report> Reports { get; set; }
+    }
+}
